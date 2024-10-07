618ed8627f87ab094a60ef0f2afe6904506fd545
steve mavuela <stevemavuela@gmail.com>
2024-10-07 15:38:46 +0100

    début du truc

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from products.models import Product
+from django.contrib.auth.models import User
 
 
 
--- products/models.py
+++ products/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from entreprises.models import Entreprise
+
 
 class Product(models.Model):
-    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name='products')
+    entreprises = models.ForeignKey(Entreprise, on_delete=models.CASCADE, related_name='products')
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
