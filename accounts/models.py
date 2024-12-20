adb82649330794de44560ef8b3294d746c826a15
steve mavuela <stevemavuela@gmail.com>
2024-12-20 21:29:52 +0100

    fff

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Profil(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.IntegerField(max_length=15)
-    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='media/profile_picture', null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     is_seller = models.BooleanField(default=False)
     city = models.CharField(max_length=255, blank=True, null=True)
