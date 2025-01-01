fb1bc27b8218ae8d10ffb341c35fb29e2d80fe4b
steve mavuela <stevemavuela@gmail.com>
2025-01-01 17:31:59 +0100

    debut

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profil
 
 
 
-class loginForm(forms.Form):
+class LoginForm(forms.Form):
     username = forms.CharField(
         max_length=15, 
         required=True, 
@@ -28,3 +28,71 @@ class loginForm(forms.Form):
         self.fields['password'].help_text = ''
         self.fields['password'].label = 'Mot de passe'
 
+
+# Custom registration form that extends UserCreationForm
+class RegistrationForm(UserCreationForm):
+    # Define choices for commune field
+    CITY_CHOICES = [
+        ('Kinshasa', 'Kinshasa'),
+        ('kisangani', 'kisangani'),
+        ('Lubumbashi', 'Lubumbashi'),
+        ('Goma', 'Goma'),
+    ]
+    
+    # Add custom fields for phone number and commune
+    phone_number = forms.CharField(
+        max_length=15, 
+        required=False, 
+        label='Numéro de téléphone', 
+        widget=forms.NumberInput(attrs={'class': 'form-control my-3', 'id': 'phoneNumberInput'}), 
+        help_text=''
+    )
+    commune = forms.ChoiceField(
+        choices=CITY_CHOICES,
+        required=False, 
+        label='City', 
+        widget=forms.Select(attrs={'class': 'form-control my-3', 'id': 'communeInput'}), 
+        help_text=''
+    )
+
+    # Meta class to specify model and form fields
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'phone_number', 'commune', 'password1', 'password2']
+        labels = {
+            'username': 'Nom d\'utilisateur',
+            'email': 'Email',
+            'password1': 'Mot de passe',
+            'password2': 'Confirmation du mot de passe',
+        }
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control my-3', 'id': 'usernameInput'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control my-3', 'id': 'emailInput'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control my-3', 'id': 'passwordInput'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control my-3', 'id': 'confirmPasswordInput'}),
+        }
+        help_texts = {
+            'username': '',
+            'email': '',
+            'phone_number': '',
+            'ville': '',
+        }
+
+    # Custom initialization of the form
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs.update({'class': 'form-control my-3', 'id': 'passwordInput'})
+        self.fields['password1'].help_text = 'Le mot de passe doit contenir au moins 8 caractères, dont une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial.'
+        self.fields['password1'].label = 'Mot de passe'
+        self.fields['password2'].label = 'Confirmation du mot de passe'
+        self.fields['password2'].help_text = ''
+        self.fields['password2'].widget.attrs.update({'class': 'form-control my-3', 'id': 'confirmPasswordInput'})
+
+    # Custom save method to handle profile creation
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        if commit:
+            user.save()
+            profile = Profil(user=user, phone_number=self.cleaned_data['phone_number'], commune=self.cleaned_data['city'])
+            profile.save()
+        return user
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages 
-from .forms import loginForm
+from .forms import LoginForm, RegistrationForm
+from validate_email_address import validate_email
+from django.shortcuts import redirect
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from django.utils.translation import gettext as _
+
+
 
 
 # Create your views here.
@@ -9,5 +15,48 @@ def login(request):
     
     pass
 
+# User registration view
 def register(request):
-    pass
+    form = RegistrationForm()
+    if request.method == 'POST':
+        if validate_email(request.POST.get('email')):
+            form = RegistrationForm(request.POST)
+            if form.is_valid():
+                try:
+                    # Create user, send welcome email, and log in
+                    user = form.save()
+                    # Use ThreadPoolExecutor to run send_welcome_email with a timeout
+                    # with concurrent.futures.ThreadPoolExecutor() as executor:
+                    #     future = executor.submit(send_welcome_email, user)
+                    #     try:
+                    #         future.result(timeout=5)  # Wait for 5 seconds
+                    #     except concurrent.futures.TimeoutError:
+                    #         # If it takes more than 5 seconds, we just continue
+                    #         pass
+                    
+                    login(request, user)
+                    messages.success(request, _('Account created successfully!'))
+                    return redirect('main:index')
+                except Exception as e:
+                    messages.warning(request, _('Error creating account:')) 
+            else:
+                # Handle form validation errors
+                for field, errors in form.errors.items():
+                    for error in errors:
+                        if 'username' in error:
+                            messages.warning(request, _('Username already taken, please choose another.'))
+                            return render(request, 'accounts/register.html', {'form': form})
+                        elif 'email' in error:
+                            messages.warning(request, _('Email address already in use, please choose another.'))
+                            return render(request, 'accounts/register.html', {'form': form})
+                        elif 'password' in error:
+                            messages.warning(request, _('Password error: ') + _(error))
+                            return render(request, 'accounts/register.html', {'form': form})
+                        else:
+                            messages.warning(request, _('erreur au champ mot de passe: {error}').format(field=field, error=_(error)))
+                return render(request, 'accounts/register.html', {'form': form})
+        else:
+            messages.warning(request, _('The email address is not valid.'))
+            return render(request, 'accounts/register.html', {'form': form})
+    return render(request, 'accounts/register.html', {'form': form})
+
