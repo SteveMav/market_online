b11800a97fee22c3bfd21b4870d8e0fc061768a6
steve mavuela <stevemavuela@gmail.com>
2025-02-22 17:56:12 +0100

    je

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from products.models import Product
 
+
 def index(request):
     products = Product.objects.all()
     return render(request, 'main/index.html', {'products': products})
